6f9af7491e4be8000e1d1a219a15ca3511377c14
Catherine <catherineep4189@gmail.com>
2020-04-19 20:39:21 +0100

    declaring a number variable and arithmetic operators

---
+++ BASICS/numbers.js
@@ -0,0 +1,16 @@
+// Data Type: Numbers
+
+let num = 5.5
+
+console.log(num)
+
+// Arithmetic operators
+// + : Addition
+// - : Subtraction
+// * : Multiplication
+// ** : Exponentiation / To The Power of...
+// / : Division
+// % : Modulus / Remainder 
+// ++ : Increment
+// -- : Decrement
+

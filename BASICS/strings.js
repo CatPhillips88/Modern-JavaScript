b1ef19925a1003835dbe429bcae413e4321eb9fc
Catherine <catherineep4189@gmail.com>
2020-04-19 20:10:27 +0100

    String Exercise Practice

--- BASICS/strings.js
+++ BASICS/strings.js
@@ -14,6 +14,21 @@ let fullName = firstName + ' ' + lastName;
 
 console.log(fullName);
 
+// CHALLENGE ONE
+
+// Make 3 variables: City, Country & Location
+// Location will be the concatenation of both city and country variable values stored 
+// with a comma between.
+// For example: Berlin, Germany 
+
+let city = 'Antwerp'
+
+let country = 'Belgium'
+
+let location = city + ', ' +  country
+
+console.log(location)
+
 
 
 

b3b5ca44fa2a7f62345444f775384c905836893b
Catherine <catherineep4189@gmail.com>
2020-04-19 19:54:49 +0100

    String Concatenation with vari able declarations

--- BASICS/strings.js
+++ BASICS/strings.js
@@ -5,6 +5,14 @@ let name = 'Catherine Phillips'
 
 console.log(name);
 
+// String Concatenation with variable declarations
+let firstName = 'Cat';
+
+let lastName = 'Phillips';
+
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
 
 
 

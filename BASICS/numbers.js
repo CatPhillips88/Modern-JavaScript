4c00fbaeaf9ad0ac57a27d62b3bc6887e966d973
Catherine <catherineep4189@gmail.com>
2020-04-20 16:00:48 +0100

    Further details on variables

--- BASICS/numbers.js
+++ BASICS/numbers.js
@@ -13,7 +13,7 @@
 
 // With bracket order of operations
 
-// let x = 11 
+// let x = 11;
 // let num = (x + 1) * 2;
 
 // console.log(num) // Result: 24
@@ -21,7 +21,7 @@
 
 // Without bracket order of operations
 
-// let x = 11 
+// let x = 11;
 // let num = x + 1 * 2;
 
 // console.log(num) // Result: 13
@@ -33,8 +33,8 @@
 // Another variable percentScore should divide both studentScore & maxScore 
 // then multiply by 100
 
-let studentScore = 14
-let maxScore = 20
-let percentScore = (studentScore / maxScore) * 100
+let studentScore = 18;
+let maxScore = 20;
+let percentScore = (studentScore / maxScore) * 100;
 
 console.log(percentScore)

8acf21e432c05f3cd3899ed76c0d43c599b04598
Catherine <catherineep4189@gmail.com>
2020-04-20 16:13:14 +0100

    converting fahrenheit to kelvin

--- BASICS/temp-conversion.js
+++ BASICS/temp-conversion.js
@@ -4,8 +4,11 @@
 // Calculate celsius and kelvin temperatures converted from fahrenheit which will be stored in variables
 
 let fahrenheit = 32;
-let celsius = (fahrenheit - 32) * (5/9)
+
+let celsius = (fahrenheit - 32) * (5/9);
 console.log(celsius) // 0°C
 
+let kelvin = (fahrenheit + 459.67) * (5/9);
+console.log(kelvin) // 273.15K
 
 

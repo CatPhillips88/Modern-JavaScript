5b0ee709f8cf2a9d659faafc647e73cebe8c9d9f
Catherine <catherineep4189@gmail.com>
2020-04-22 18:14:48 +0100

    arguments with challenge practice

--- FUNCTIONS/arguments.js
+++ FUNCTIONS/arguments.js
@@ -2,9 +2,33 @@
 
 // Multiple Arguments
 
-let add = function (a, b) {
-   return a + b
+let add = function (a, b, c) {
+    return a + b + c
 }
 
-let result = add(10, 1)
+let result = add(10, 1, 5)
 console.log(result)
+
+
+// Default Arguments
+// If no argument value into the function then we can set a default value that is used instead.
+// The example uses a number and string but we can use other data types
+
+
+let getScoreText = function (name = 'Anonymous', score = 0) {
+    return 'Name: ' + name + ' - Score: ' + score
+}
+
+let scoreText = getScoreText(undefined, 99)
+console.log(scoreText)
+
+// CHALLENGE NINE
+
+// Tip Calculator: create a function that calculates a tip percentage (20%) added to a restaurant bill
+
+let calculateTip = function (total, tipPercent = .2) {
+    return total * tipPercent
+}
+
+let getTip = calculateTip(100)
+console.log(getTip)

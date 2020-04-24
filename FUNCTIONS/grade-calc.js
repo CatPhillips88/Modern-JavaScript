85f9042927eb4f43e2751c425f337eb0ad61f426
Catherine <catherineep4189@gmail.com>
2020-04-24 22:48:42 +0100

    refactoring code

--- FUNCTIONS/grade-calc.js
+++ FUNCTIONS/grade-calc.js
@@ -10,20 +10,37 @@
 
 let totalScore = function (studentScore, totalScore = 20) {
     let scoreCalculation = (studentScore / totalScore) * 100
+    let letterGrade = ''
+
+    //     if (scoreCalculation >= 90) {
+    //         return `You got a A (${scoreCalculation}%)`
+    //     } else if (scoreCalculation >= 80) {
+    //         return `You got a B (${scoreCalculation}%)`
+    //     } else if (scoreCalculation >= 70) {
+    //         return `You got a C (${scoreCalculation}%)`
+    //     } else if (scoreCalculation >= 60) {
+    //         return `You got a D (${scoreCalculation}%)`
+    //     } else {
+    //         return `You got a E (${scoreCalculation}%)`
+    //     }
+    // }
 
     if (scoreCalculation >= 90) {
-        return `You got a A (${scoreCalculation}%)`
+        letterGrade = 'A'
     } else if (scoreCalculation >= 80) {
-        return `You got a B (${scoreCalculation}%)`
+        letterGrade = 'B'
     } else if (scoreCalculation >= 70) {
-        return `You got a C (${scoreCalculation}%)`
+        letterGrade = 'C'
     } else if (scoreCalculation >= 60) {
-        return `You got a D (${scoreCalculation}%)`
+        letterGrade = 'D'
     } else {
-        return `You got a E (${scoreCalculation}%)`
+        letterGrade = 'E'
     }
+
+    return `You got a ${letterGrade} (${scoreCalculation}%) `
 }
 
-let results = totalScore(0)
-console.log(results)
+
+    let results = totalScore(0)
+    console.log(results)
 

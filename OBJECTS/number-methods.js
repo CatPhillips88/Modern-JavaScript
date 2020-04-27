a8a491c0b03fb589ddf627ac87d945879e4efeac
Catherine <catherineep4189@gmail.com>
2020-04-27 15:39:25 +0100

    challenge exercise

--- OBJECTS/number-methods.js
+++ OBJECTS/number-methods.js
@@ -26,7 +26,7 @@ let randomNum = Math.random()
 console.log(randomNum)
 
 // if we don't want to generate a number between 0 and 1 
-// we can use the equation below
+// we can use the equation below for any range
 
 // range conversion from 0 to 1 TO 10 to 20
 // 0 - 0.999999 (initial start)
@@ -40,3 +40,23 @@ let max = 20
 
 let otherNum = Math.floor(Math.random() * (max - min + 1)) + min
 console.log(otherNum)
+
+// CHALLENGE FIFTEEN 
+
+// create a game which guesses a persons number 
+
+// passing in a single argument 
+// generate a random number in a given range then guess whether correct or not
+// range: 1 - 5 true if correct or false if not correct
+
+let makeGuess = function (num) {
+    let min = 1
+    let max = 5
+    let guessNum = Math.floor(Math.random() * (max - min + 1)) + min
+
+    console.log(guessNum)
+    return num === guessNum
+
+}
+
+console.log(makeGuess(2))

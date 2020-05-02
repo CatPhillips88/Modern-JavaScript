bb6a260c4e5f6b16b680f52a41b775233400c897
Catherine <catherineep4189@gmail.com>
2020-05-02 17:41:16 +0100

    array search methods

--- ARRAYS/notes.js
+++ ARRAYS/notes.js
@@ -94,6 +94,8 @@
 //    console.log(notes[count])
 // }
 
+
+
 // indexOf (method) 
 
 // the indexOf method returns the first index of an element in the array
@@ -102,15 +104,23 @@
 // console.log(notes.indexOf('Note 2')) // returns 1
 // console.log(notes.indexOf('note 2')) // returns -1 owing to case sensitivity
 
+
 // findIndex (method) returns index of first element in array when a test condition via a function
 // is satsified true or not false
 
-// pseudocode: using the findIndex to execute a callback function where we search for a match within 
+// pseudocode (EXAMPLE 1): using the findIndex to execute a callback function where we search for a match within 
 // the notes array of objects. 
 // the argument (item) is used with dot notation to access object property value to compare using the 
 // triple equals operator to search for a match and return its index which in this example is 3
 
-const notes = [{}, {
+// pseudocode (EXAMPLE 2): in this example rather than search for a match and returning its index position
+// we've used a function with two arguments (notes - the array) and noteTitle which will pass a string
+// we want to query using the comparision operator within findIndex method which returns a boolean value.
+// If true, then object within the array will return however if false, returns undefined
+// we added toLowerCase string method for case insensitivity
+
+
+const notes = [{
    title: 'My next trip',
    body: 'I would like to go to Califonia'
 }, {
@@ -121,12 +131,25 @@ const notes = [{}, {
    body: 'Get a new seat'
 }]
 
-const index = notes.findIndex(function (item, index) {
-  return item.body === 'Get a new seat'
-})
+// findIndex EXAMPLE 1
 
-console.log(index)
+// const index = notes.findIndex(function (item, index) {
+//   return item.body === 'Get a new seat'
+// })
 
+// console.log(index)
+
+// findIndex EXAMPLE 2
+
+const findNote = function (notes, noteTitle) {
+   const index = notes.findIndex(function (note, index) {
+      return note.title.toLowerCase() === noteTitle.toLowerCase()
+   })
+   return notes[index]
+}
+
+const note = findNote(notes, 'Office modification')
+console.log(note)
 
 
 
